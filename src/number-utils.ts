84fe51caa994903b7ebef10421528371d3ead3b3
Jenii23 <jenifferal23972@hotmail.com>
2023-10-28 12:17:35 -0500

    JenifferAlcivar/Tarea#5/funcionMultiplicar/Corrección de errores

--- src/number-utils.ts
+++ src/number-utils.ts
@@ -8,6 +8,7 @@ export const sum = (a: number, b: number) => {
 export const multiply = (a: number, b: number) => {
   return a * b;
 }
+
 export const divide = (a: number, b: number): number => {
   if (b === 0) {
     throw new Error('You can\'t divide by zero');
--- test/number-utils.test.ts
+++ test/number-utils.test.ts
@@ -1,6 +1,7 @@
 import { sum, multiply, divide} from '../src/number-utils';
 
 import { firstValue } from '../src/array-utils';
+
 describe('blah', () => {
   it('works', () => {
     expect(sum(1, 1)).toEqual(2);
@@ -12,14 +13,17 @@ describe('multiply', () => {
     expect(multiply(1, 1)).toEqual(1);
   });
 });
+
 describe('divide', () => {
   it('should divide two numbers correctly', () => {
     expect(divide(6, 3)).toEqual(2);
   });
 });
-  it('should handle division by zero', () => {
+
+it('should handle division by zero', () => {
     expect(() => divide(5, 0)).toThrow("You can't divide by zero");
-  });
+});
+
 describe('firstValue', () => {
   it('should get the first value of an array', () => {
     const mockArray = ['Roberto', 'Loayza'];
