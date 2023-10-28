c50e1a927df29fe9e0bd010a6a23df5a29a52d38
garyvera <108149433+garyvera@users.noreply.github.com>
2023-10-28 12:56:22 -0500

    Funcion que convierte una cadena de caracteres a mayuscula (#33)
    * Funcion cambiar cadena de caracteres a mayuscula
    
    * Correción parte del equipo
    
    * Revert "Funcion cambiar cadena de caracteres a mayuscula (#25)"
    
    This reverts commit d64d4d35467c7f46dbcd3138197ea5f436d40a4e.
    
    * Correcion nombre de funcion
    
    * Cambio nombre de funcion

--- src/string-utils.ts
+++ src/string-utils.ts
@@ -1,3 +1,7 @@
 export const letterLast = (palabra: string) => {
   return palabra.charAt(palabra.length - 1);
 };
+
+export function convertStringToUpperCase(str: string) {
+  return str.toUpperCase();
+}
