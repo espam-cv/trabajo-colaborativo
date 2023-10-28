238720df558d3641944bdd76bf47eb90cb30fade
JonathanOrmaza <jonathan.ormaza@espam.edu.ec>
2023-10-28 12:06:41 -0500

    Archivo string test

--- test/number-utils.test.ts
+++ test/number-utils.test.ts
@@ -1,5 +1,5 @@
 import { sum } from '../src/number-utils';
-import { letterLast } from '../src/string-utils';
+
 describe('blah', () => {
   it('works', () => {
     expect(sum(1, 1)).toEqual(2);
@@ -7,8 +7,3 @@ describe('blah', () => {
 });
 
 
-describe('letterLast', () => {
-  it('should palabra hola return a', () => {
-    expect(letterLast("hola")).toEqual('a');
-  });
-});
