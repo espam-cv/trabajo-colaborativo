1b9aad5d3a75db76c81281735afb5d0ad5c5abb5
Nancy Vera <nancyestefaniavs@gmail.com>
2023-10-28 10:06:01 -0500

    The divide function was carried out with its respective test NV

--- src/number-utils.ts
+++ src/number-utils.ts
@@ -4,3 +4,13 @@ export const sum = (a: number, b: number) => {
   }
   return a + b;
 };
+
+export const divide = (a: number, b: number): number => {
+  if (b === 0) {
+    if (process.env.NODE_ENV === 'development') {
+      console.log('You can\'t divide by zero');
+    }
+    throw new Error('You can\'t divide by zero');
+  }
+  return a / b;
+};
--- test/number-utils.test.ts
+++ test/number-utils.test.ts
@@ -5,3 +5,16 @@ describe('blah', () => {
     expect(sum(1, 1)).toEqual(2);
   });
 });
+
+
+import { divide } from '../src/number-utils'; 
+
+describe('divide', () => {
+  it('should divide two numbers correctly', () => {
+    expect(divide(6, 3)).toEqual(2);
+  });
+
+  it('should handle division by zero', () => {
+    expect(() => divide(5, 0)).toThrow("You can't divide by zero");
+  });
+});
