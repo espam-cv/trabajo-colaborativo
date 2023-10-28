a02b96d5a1ca3be625b9e0a63e7d4f86cbfe8292
Fabricio Flores <fabrifloresg@gmail.com>
2023-10-28 10:51:15 -0500

    No ticket/revert no reviewed changes (#27)
    * Revert "funcion-devolver-primera-letra (#24)"
    
    This reverts commit df0a6919f06ea970c1159c7b9a69447e48782a6a.
    
    * Revert "funcion multiplicar (#23)"
    
    This reverts commit d2c02f53ad703ac6c1ab94191836611457ce515c.

--- test/number-utils.test.ts
+++ test/number-utils.test.ts
@@ -1,16 +1,7 @@
-import { sum, multiply} from '../src/number-utils';
-
-
+import { sum } from '../src/number-utils';
 
 describe('blah', () => {
   it('works', () => {
     expect(sum(1, 1)).toEqual(2);
-  });
-});
-
-
-describe('multiply', () => {
-  it('should return multiplication of two numbers', () => {
-    expect(multiply(1, 1)).toEqual(1);
   });
 });
