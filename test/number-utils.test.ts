1f958d02a60b493821931753194e9ce0dc155161
Jenii23 <jenifferal23972@hotmail.com>
2023-10-28 10:16:17 -0500

    funcion multiplicar

--- test/number-utils.test.ts
+++ test/number-utils.test.ts
@@ -1,7 +1,16 @@
-import { sum } from '../src/number-utils';
+import { sum, multiply} from '../src/number-utils';
+
+
 
 describe('blah', () => {
   it('works', () => {
     expect(sum(1, 1)).toEqual(2);
+  });
+});
+
+
+describe('multiply', () => {
+  it('should return multiplication of two numbers', () => {
+    expect(multiply(1, 1)).toEqual(1);
   });
 });
