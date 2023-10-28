904c00e01bf53478dcb588d1e1d9fb1d4f2102fc
Nancy <51144103+NancyEVeSo@users.noreply.github.com>
2023-10-28 12:04:11 -0500

    crear función para dividir#5 - The divide function was carried out with its respective test NV (#22)
    * The divide function was carried out with its respective test NV
    
    * crear función para dividir#5 - Correciones nv

--- src/number-utils.ts
+++ src/number-utils.ts
@@ -4,3 +4,9 @@ export const sum = (a: number, b: number) => {
   }
   return a + b;
 };
+export const divide = (a: number, b: number): number => {
+  if (b === 0) {
+    throw new Error('You can\'t divide by zero');
+  }
+  return a / b;
+};
--- test/number-utils.test.ts
+++ test/number-utils.test.ts
@@ -1,15 +1,23 @@
-import { sum } from '../src/number-utils';
-import { firstValue } from '../src/array-utils';
 
+import { sum, divide } from '../src/number-utils';
+import { firstValue } from '../src/array-utils';
 describe('blah', () => {
   it('works', () => {
     expect(sum(1, 1)).toEqual(2);
   });
 });
-
+describe('divide', () => {
+  it('should divide two numbers correctly', () => {
+    expect(divide(6, 3)).toEqual(2);
+  });
+});
+  it('should handle division by zero', () => {
+    expect(() => divide(5, 0)).toThrow("You can't divide by zero");
+  });
 describe('firstValue', () => {
   it('should get the first value of an array', () => {
     const mockArray = ['Roberto', 'Loayza'];
     expect(firstValue(mockArray)).toEqual('Roberto');
   });
 });
+
