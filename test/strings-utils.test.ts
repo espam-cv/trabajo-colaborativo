2ee4319ed082895a65883ba3abc091dd8ed7e2e7
Gary Vera <garyveralucas@gmail.com>
2023-10-28 12:53:22 -0500

    Cambio nombre de funcion

--- test/strings-utils.test.ts
+++ test/strings-utils.test.ts
@@ -1,8 +1,14 @@
 
-import { letterLast } from '../src/string-utils';
+import { letterLast, convertStringToUpperCase } from '../src/string-utils';
 
 describe('letterLast', () => {
     it('should palabra hola return a', () => {
       expect(letterLast("hola")).toEqual('a');
     });
   });
+
+describe('convertStringToUpperCase', () => {
+  it('should word hola return HOLA', () => {
+    expect(convertStringToUpperCase("hola")).toEqual('HOLA');
+  });
+});
