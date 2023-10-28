19237aa2d23d65ab039764e22835b4cdc7b265a1
Nancy Vera <nancyestefaniavs@gmail.com>
2023-10-28 11:14:07 -0500

    crear función para dividir#5 - Correciones nv

--- src/number-utils.ts
+++ src/number-utils.ts
@@ -4,12 +4,8 @@ export const sum = (a: number, b: number) => {
   }
   return a + b;
 };
-
 export const divide = (a: number, b: number): number => {
   if (b === 0) {
-    if (process.env.NODE_ENV === 'development') {
-      console.log('You can\'t divide by zero');
-    }
     throw new Error('You can\'t divide by zero');
   }
   return a / b;
--- test/number-utils.test.ts
+++ test/number-utils.test.ts
@@ -1,14 +1,10 @@
-import { sum } from '../src/number-utils';
+import { sum, divide } from '../src/number-utils';
 
 describe('blah', () => {
   it('works', () => {
     expect(sum(1, 1)).toEqual(2);
   });
 });
-
-
-import { divide } from '../src/number-utils'; 
-
 describe('divide', () => {
   it('should divide two numbers correctly', () => {
     expect(divide(6, 3)).toEqual(2);
