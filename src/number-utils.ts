e04c7a13b8e8b50bc94105ebaf9bbb708b3b0e4d
Jenii23 <33467235+Jenii23@users.noreply.github.com>
2023-10-28 12:41:40 -0500

    #4 - Crear función de multiplicar dos números (#30)
    * funcion multiplicar
    
    * JenifferAlcivar/Tarea#5/funcionMultiplicar
    
    * JenifferAlcivar/Tarea#5/funcionMultiplicar/Eliminacion de comentario
    
    * JenifferAlcivar/Tarea#5/funcionMultiplicar/Corrección de errores
    
    * JenifferAlcivar/Tarea#5/funcionMultiplicar/Corrección de espacios
    
    * JenifferAlcivar/Tarea#5/funcionMultiplicar/Corrección de espacios
    
    * JenifferAlcivar/Tarea#5/funcionMultiplicar/Corrección de espacios
    
    ---------
    
    Co-authored-by: Jenii23 <jenifferal23972@hotmail.com>

--- src/number-utils.ts
+++ src/number-utils.ts
@@ -4,6 +4,11 @@ export const sum = (a: number, b: number) => {
   }
   return a + b;
 };
+
+export const multiply = (a: number, b: number) => {
+  return a * b;
+}
+
 export const divide = (a: number, b: number): number => {
   if (b === 0) {
     throw new Error('You can\'t divide by zero');
--- test/number-utils.test.ts
+++ test/number-utils.test.ts
@@ -1,19 +1,27 @@
-
-import { sum, divide } from '../src/number-utils';
+import { sum, multiply, divide} from '../src/number-utils';
 import { firstValue } from '../src/array-utils';
+
 describe('blah', () => {
   it('works', () => {
     expect(sum(1, 1)).toEqual(2);
   });
 });
+
+describe('multiply', () => {
+  it('should return multiplication of two numbers', () => {
+    expect(multiply(1, 1)).toEqual(1);
+  });
+});
+
 describe('divide', () => {
   it('should divide two numbers correctly', () => {
     expect(divide(6, 3)).toEqual(2);
   });
-});
-  it('should handle division by zero', () => {
+    it('should handle division by zero', () => {
     expect(() => divide(5, 0)).toThrow("You can't divide by zero");
   });
+});
+
 describe('firstValue', () => {
   it('should get the first value of an array', () => {
     const mockArray = ['Roberto', 'Loayza'];
