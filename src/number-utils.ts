d2c02f53ad703ac6c1ab94191836611457ce515c
Jenii23 <33467235+Jenii23@users.noreply.github.com>
2023-10-28 10:19:49 -0500

    funcion multiplicar (#23)
    Co-authored-by: Jenii23 <jenifferal23972@hotmail.com>

--- src/number-utils.ts
+++ src/number-utils.ts
@@ -4,3 +4,8 @@ export const sum = (a: number, b: number) => {
   }
   return a + b;
 };
+// función de multiplicar dos números
+export const multiply = (a: number, b: number) => {
+
+  return a * b;
+};
