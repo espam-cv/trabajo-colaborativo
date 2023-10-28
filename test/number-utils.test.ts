01e8120c5e275fba7fad2b30ce8400024772f171
Jenii23 <jenifferal23972@hotmail.com>
2023-10-28 12:40:19 -0500

    JenifferAlcivar/Tarea#5/funcionMultiplicar/Corrección de espacios

--- test/number-utils.test.ts
+++ test/number-utils.test.ts
@@ -1,5 +1,4 @@
 import { sum, multiply, divide} from '../src/number-utils';
-
 import { firstValue } from '../src/array-utils';
 
 describe('blah', () => {
@@ -18,7 +17,7 @@ describe('divide', () => {
   it('should divide two numbers correctly', () => {
     expect(divide(6, 3)).toEqual(2);
   });
-  it('should handle division by zero', () => {
+    it('should handle division by zero', () => {
     expect(() => divide(5, 0)).toThrow("You can't divide by zero");
   });
 });
