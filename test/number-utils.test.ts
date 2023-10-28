40a35b57ea5525287c88a743ebf03d80cb25fc77
Jorge Loayza <rob_cuero@hotmail.es>
2023-10-28 09:52:50 -0500

    #8 obtener el primer valor de array

--- test/number-utils.test.ts
+++ test/number-utils.test.ts
@@ -1,7 +1,13 @@
 import { sum } from '../src/number-utils';
+import { primerValor } from '../src/array-utils';
 
+const mockArray = ['Roberto', 'Loayza'];
 describe('blah', () => {
   it('works', () => {
     expect(sum(1, 1)).toEqual(2);
+  });
+
+  it('shpuld get the first value of an array', () => {
+    expect(primerValor(mockArray)).toEqual('Roberto');
   });
 });
