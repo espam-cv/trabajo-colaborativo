d48fcbd619b69dd3dd9511a772dfd10a0b5bcb40
Jenii23 <jenifferal23972@hotmail.com>
2023-10-28 12:31:55 -0500

    JenifferAlcivar/Tarea#5/funcionMultiplicar/Corrección de espacios

--- test/number-utils.test.ts
+++ test/number-utils.test.ts
@@ -12,16 +12,16 @@ describe('multiply', () => {
   it('should return multiplication of two numbers', () => {
     expect(multiply(1, 1)).toEqual(1);
   });
+
+  it('should handle division by zero', () => {
+    expect(() => divide(5, 0)).toThrow("You can't divide by zero");
+});
 });
 
 describe('divide', () => {
   it('should divide two numbers correctly', () => {
     expect(divide(6, 3)).toEqual(2);
   });
-});
-
-it('should handle division by zero', () => {
-    expect(() => divide(5, 0)).toThrow("You can't divide by zero");
 });
 
 describe('firstValue', () => {
