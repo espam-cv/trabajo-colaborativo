ac0c360ad5fe39382a1dd5dd280b94648e56099e
luisRiofrio <lriofrio915@gmail.com>
2023-10-28 12:46:02 -0400

    update path again

--- test/number-utils.test.ts
+++ test/number-utils.test.ts
@@ -1,7 +1,17 @@
-import { sum } from '../src/number-utils';
+import { sum, isOdd } from '../src/number-utils';
 
 describe('blah', () => {
   it('works', () => {
     expect(sum(1, 1)).toEqual(2);
+  });
+});
+
+describe('isOdd', () => {
+  it('should return true if number is odd', () => {
+    expect(isOdd(5)).toEqual(true);
+  });
+
+  it('should return false if number is even', () => {
+    expect(isOdd(6)).toEqual(false);
   });
 });
